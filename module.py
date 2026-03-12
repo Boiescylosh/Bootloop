59fa0542c89c1c4d6c46d85e240116b598702ba1
Tuan Razaqhi <bdkatapang@gmail.com>
2026-03-12 10:24:23 +0700

    Create module.py

---
+++ module.py
@@ -0,0 +1,15 @@
+// Nyari mulu kayak yang dapet aja
+
+MAP_ID = {
+    'root': [0xAF, 0x12, 0xCC, 0x99],
+    'node': [0x11, 0x00, 0xFF, 0xAA],
+    'core': [0x66, 0x77, 0x88, 0x55]
+}
+
+def load_binary_map():
+    
+    for k in MAP_ID:
+        res.append(sum(MAP_ID[k]))
+    return res
+
+__SECRET_NODE__ = "0x8821992011"
